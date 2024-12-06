c47260ee7a3cfe00f52c3df2bf23a9d9ccb5aa36
user <nikitaskrypets@gmail.com>
2024-12-06 16:40:10 +0200

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import io
+
+from django.db.migrations import serializer
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer_to_json = CarSerializer(car)
+    return JSONRenderer().render(serializer_to_json.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    return JSONParser().parse(stream)
